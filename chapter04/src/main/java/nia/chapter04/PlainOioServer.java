5bbb2dd22c917cb5925d882f736cb94c335c8a03
王俊超 <shining-glory@qq.com>
2017-10-13 08:33:56 +0800

    commit

---
+++ chapter04/src/main/java/nia/chapter04/PlainOioServer.java
@@ -0,0 +1,50 @@
+package nia.chapter04;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-10-13 08:23
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class PlainOioServer {
+    public void serve(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for (; ; ) {
+                final Socket clientSocket = socket.accept();
+                System.out.println(
+                        "Accepted connection from " + clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\r\n".getBytes(
+                                    Charset.forName("UTF-8")));
+                            out.flush();
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException ex) {
+                                // ignore on close
+                            }
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
