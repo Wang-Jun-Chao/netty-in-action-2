2516a7a47fa1bf4ef9902a2db4b2367c94a8a530
王俊超 <shining-glory@qq.com>
2017-10-15 22:58:27 +0800

    commit

---
+++ chapter07/src/main/java/nia/chapter07/EventLoopExamples.java
@@ -0,0 +1,40 @@
+package nia.chapter07;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-10-15 22:53
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class EventLoopExamples {
+    /**
+     * Listing 7.1 Executing tasks in an event loop
+     * */
+    public static void executeTaskInEventLoop() {
+        boolean terminated = true;
+        //...
+        while (!terminated) {
+            List<Runnable> readyEvents = blockUntilEventsReady();
+            for (Runnable ev: readyEvents) {
+                ev.run();
+            }
+        }
+    }
+
+    private static final List<Runnable> blockUntilEventsReady() {
+        return Collections.<Runnable>singletonList(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
