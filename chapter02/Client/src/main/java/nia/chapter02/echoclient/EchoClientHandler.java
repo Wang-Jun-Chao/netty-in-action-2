4adedd9f8f0da241165c30f93ca047058ea470c9
王俊超 <shining-glory@qq.com>
2017-10-18 07:27:15 +0800

    commit

--- chapter02/Client/src/main/java/nia/chapter02/echoclient/EchoClientHandler.java
+++ chapter02/Client/src/main/java/nia/chapter02/echoclient/EchoClientHandler.java
@@ -20,16 +20,19 @@ public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 当被通知Channel是活跃的时候，发送一条消息
         ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        // 记录已接收消息的转储
         System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // 在发生异常时，记录错误并关闭Channel
         cause.printStackTrace();
         ctx.close();
     }
