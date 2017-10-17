279205317e66f1adff05ba9b59204189c4a3268e
王俊超 <shining-glory@qq.com>
2017-10-17 08:07:27 +0800

    commit

--- chapter01/src/main/java/nia/chapter01/BlockingIoExample.java
+++ chapter01/src/main/java/nia/chapter01/BlockingIoExample.java
@@ -16,18 +16,27 @@ import java.net.Socket;
  */
 public class BlockingIoExample {
     public void serve(int portNumber) throws IOException {
+        // 创建一个新的ServerSocket，用以监听指定端口上的连接请求
         ServerSocket serverSocket = new ServerSocket(portNumber);
+        // 对accept()方法的调用将被阻塞，直到一个连接建立
         Socket clientSocket = serverSocket.accept();
+
+        // 流对象都派生于该套接字的流对象
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         String request;
         String response;
+
+        // 处理循环开始
         while ((request = in.readLine()) != null) {
+            // 如果客户端发送了“Done”，则退出处理循环
             if ("Done".endsWith(request)) {
                 break;
             }
 
+            // 请求被传递给服务器的处理方法
             response = processRequest(request);
+            // 服务器的响应被发送给了客户端
             out.println(response);
         }
     }
--- chapter01/src/main/java/nia/chapter01/ConnectExample.java
+++ chapter01/src/main/java/nia/chapter01/ConnectExample.java
@@ -23,16 +23,21 @@ public class ConnectExample {
     public static void connect() {
         Channel channel = CHANNEL_FROM_SOMEWHERE; // 指向某个位置
 
-        // 不会阻塞
+        // 不会阻塞，异步地连接到远程节点
         ChannelFuture future = channel.connect(new InetSocketAddress("192.168.0.1", 25));
+        // 注册一个ChannelFutureListener，以便在操作完成时获得通知
         future.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) throws Exception {
+                // 检查操作的状态
                 if (future.isSuccess()) {
+                    // 如果操作是成功的，则创建一个ByteBuf 以持有数据
                     ByteBuf buffer = Unpooled.copiedBuffer("Hello", Charset.forName("UTF-8"));
+                    // 将数据异步地发送到远程节点。返回一个ChannelFuture
                     ChannelFuture wf = future.channel().write(buffer);
                     // ...
                 } else {
+                    // 如果发生错误，则访问描述原因的Throwable
                     Throwable cause = future.cause();
                     cause.printStackTrace();
                 }
