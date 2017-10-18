48cd64dba530d2f17c475a40dd4e6a9834e93dc7
王俊超 <shining-glory@qq.com>
2017-10-19 07:01:44 +0800

    commit

--- chapter04/src/main/java/nia/chapter04/ChannelOperationExamples.java
+++ chapter04/src/main/java/nia/chapter04/ChannelOperationExamples.java
@@ -26,14 +26,19 @@ public class ChannelOperationExamples {
      */
     public static void writingToChannel() {
         Channel channel = CHANNEL_FROM_SOMEWHERE; // Get the channel reference from somewhere
+        // 创建持有要写数据的ByteBuf
         ByteBuf buf = Unpooled.copiedBuffer("your data", CharsetUtil.UTF_8);
+        // 写数据并刷新
         ChannelFuture cf = channel.writeAndFlush(buf);
+        // 添加ChannelFutureListener 以便在写操作完成后接收通知
         cf.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) {
+                // 写操作完成，并且没有错误发生
                 if (future.isSuccess()) {
                     System.out.println("Write successful");
                 } else {
+                    // 记录错误
                     System.err.println("Write error");
                     future.cause().printStackTrace();
                 }
