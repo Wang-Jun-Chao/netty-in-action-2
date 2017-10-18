92e8f7c7ad844cbd0999001ceb03ccd41fd41694
王俊超 <shining-glory@qq.com>
2017-10-19 06:58:13 +0800

    commit

--- chapter04/src/main/java/nia/chapter04/NettyNioServer.java
+++ chapter04/src/main/java/nia/chapter04/NettyNioServer.java
@@ -21,25 +21,33 @@ import java.nio.charset.Charset;
 public class NettyNioServer {
     public void server(int port) throws Exception {
         final ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+        // 为非阻塞模式使用NioEventLoopGroup
         NioEventLoopGroup group = new NioEventLoopGroup();
         try {
+            // 创建ServerBootstrap
             ServerBootstrap b = new ServerBootstrap();
-            b.group(group).channel(NioServerSocketChannel.class)
+            b.group(group)
+                    // 指定ChannelInitializer，对于每个已接受的连接都调用它
+                    .channel(NioServerSocketChannel.class)
                     .localAddress(new InetSocketAddress(port))
+                    // 添加一个ChannelInboundHandlerAdapter 以拦截和处理事件
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                                 @Override
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    // 将消息写到客户端，并添加ChannelFutureListener，以便消息一被写完就关闭连接
                                     ctx.writeAndFlush(buf.duplicate()).addListener(ChannelFutureListener.CLOSE);
                                 }
                             });
                         }
                     });
+            // 绑定服务器以接受连接
             ChannelFuture f = b.bind().sync();
             f.channel().closeFuture().sync();
         } finally {
+            // 释放所有的资源
             group.shutdownGracefully().sync();
         }
     }
