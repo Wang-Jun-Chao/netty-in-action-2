7a3c6f4832a48a4f3b05e3b721fcb0c84c6e4c6c
王俊超 <shining-glory@qq.com>
2017-10-19 06:31:16 +0800

    commit

--- chapter04/src/main/java/nia/chapter04/PlainOioServer.java
+++ chapter04/src/main/java/nia/chapter04/PlainOioServer.java
@@ -15,16 +15,20 @@ import java.nio.charset.Charset;
  */
 public class PlainOioServer {
     public void serve(int port) throws IOException {
+        // 将服务器绑定到指定端口
         final ServerSocket socket = new ServerSocket(port);
         try {
             for (; ; ) {
+                // 接受连接
                 final Socket clientSocket = socket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
+                // 创建一个新的线程来处理该连接
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         OutputStream out;
                         try {
+                            // 将消息写给已连接的客户端
                             out = clientSocket.getOutputStream();
                             out.write("Hi!\r\n".getBytes(Charset.forName("UTF-8")));
                             out.flush();
@@ -33,13 +37,14 @@ public class PlainOioServer {
                             e.printStackTrace();
                         } finally {
                             try {
+                                // 关闭连接
                                 clientSocket.close();
                             } catch (IOException ex) {
                                 // ignore on close
                             }
                         }
                     }
-                }).start();
+                }).start(); // 启动线程
             }
         } catch (IOException e) {
             e.printStackTrace();
