032a75c9d659f3e522cb883a7e0725738d3f7898
王俊超 <shining-glory@qq.com>
2017-10-19 06:37:05 +0800

    commit

--- chapter04/src/main/java/nia/chapter04/PlainNioServer.java
+++ chapter04/src/main/java/nia/chapter04/PlainNioServer.java
@@ -24,8 +24,11 @@ public class PlainNioServer {
         serverChannel.configureBlocking(false);
         ServerSocket ss = serverChannel.socket();
         InetSocketAddress address = new InetSocketAddress(port);
+        // 将服务器绑定到选定的端口
         ss.bind(address);
+        // 打开Selector来处理Channel
         Selector selector = Selector.open();
+        // 将ServerSocket注册到Selector 以接受连接
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
         final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
         for (; ; ) {
@@ -36,27 +39,35 @@ public class PlainNioServer {
                 //handle exception
                 break;
             }
+            // 获取所有接收事件的SelectionKey 实例
             Set<SelectionKey> readyKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = readyKeys.iterator();
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 iterator.remove();
                 try {
+                    // 检查事件是否是一个新的已经就绪可以被接受的连接
                     if (key.isAcceptable()) {
                         ServerSocketChannel server = (ServerSocketChannel) key.channel();
                         SocketChannel client = server.accept();
                         client.configureBlocking(false);
+                        // 接受客户端，并将它注册到选择器
                         client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
                         System.out.println("Accepted connection from " + client);
                     }
+
+                    // 检查套接字是否已经准备好写数据
                     if (key.isWritable()) {
                         SocketChannel client = (SocketChannel) key.channel();
                         ByteBuffer buffer = (ByteBuffer) key.attachment();
                         while (buffer.hasRemaining()) {
+                            // 将数据写到已连接的客户端
                             if (client.write(buffer) == 0) {
                                 break;
                             }
                         }
+
+                        // 关闭连接
                         client.close();
                     }
                 } catch (IOException ex) {
