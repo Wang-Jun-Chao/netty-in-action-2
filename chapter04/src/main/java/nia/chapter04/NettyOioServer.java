96c5ed7a6b1e387ca32381d4e1167b1045db8ec4
王俊超 <shining-glory@qq.com>
2017-10-19 06:46:16 +0800

    commit

--- chapter04/src/main/java/nia/chapter04/NettyOioServer.java
+++ chapter04/src/main/java/nia/chapter04/NettyOioServer.java
@@ -19,29 +19,35 @@ import java.nio.charset.Charset;
  * All Rights Reserved !!!
  */
 public class NettyOioServer {
-    public void server(int port)
-            throws Exception {
+    public void server(int port) throws Exception {
         final ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
         EventLoopGroup group = new OioEventLoopGroup();
         try {
+            // 创建ServerBootstrap
             ServerBootstrap b = new ServerBootstrap();
             b.group(group)
+                    // 使用OioEventLoopGroup以允许阻塞模式（旧的I/O）
                     .channel(OioServerSocketChannel.class)
                     .localAddress(new InetSocketAddress(port))
+                    // 指定ChannelInitializer，对于每个已接受的连接都调用它
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
+                            // 添加一个ChannelInboundHandlerAdapter 以拦截和处理事件
                             ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                                 @Override
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    // 将消息写到客户端，并添加ChannelFutureListener，以便消息一被写完就关闭连接
                                     ctx.writeAndFlush(buf.duplicate()).addListener(ChannelFutureListener.CLOSE);
                                 }
                             });
                         }
                     });
+            // 绑定服务器接受连接
             ChannelFuture f = b.bind().sync();
             f.channel().closeFuture().sync();
         } finally {
+            // 释放所有的资源
             group.shutdownGracefully().sync();
         }
     }
