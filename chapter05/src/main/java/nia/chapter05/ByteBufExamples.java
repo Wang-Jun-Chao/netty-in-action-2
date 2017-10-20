4fcc209cb966daf0d8a3b0b5f0b91b9313701ec0
王俊超 <shining-glory@qq.com>
2017-10-21 06:51:37 +0800

    commit

--- chapter05/src/main/java/nia/chapter05/ByteBufExamples.java
+++ chapter05/src/main/java/nia/chapter05/ByteBufExamples.java
@@ -166,10 +166,15 @@ public class ByteBufExamples {
      */
     public static void byteBufSlice() {
         Charset utf8 = Charset.forName("UTF-8");
+        // 创建一个用于保存给定字符串的字节的ByteBuf
         ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", utf8);
+        // 创建该ByteBuf 从索引0 开始到索引15结束的一个新切片
         ByteBuf sliced = buf.slice(0, 15);
+        // 将打印“Netty in Action”
         System.out.println(sliced.toString(utf8));
+        // 更新索引0 处的字节
         buf.setByte(0, (byte) 'J');
+        // 将会成功，因为数据是共享的，对其中一个所做的更改对另外一个也是可见的
         assert buf.getByte(0) == sliced.getByte(0);
     }
 
