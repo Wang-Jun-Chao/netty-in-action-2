af9d02ce5384254368ecc54e8c0c12035dec487b
王俊超 <shining-glory@qq.com>
2017-10-23 12:01:09 +0800

    commit

--- chapter07/src/main/java/nia/chapter07/EventLoopExamples.java
+++ chapter07/src/main/java/nia/chapter07/EventLoopExamples.java
@@ -18,8 +18,10 @@ public class EventLoopExamples {
         boolean terminated = true;
         //...
         while (!terminated) {
+            // 阻塞，直到有事件已经就绪可被运行
             List<Runnable> readyEvents = blockUntilEventsReady();
             for (Runnable ev: readyEvents) {
+                // 循环遍历，并处理所有的事件
                 ev.run();
             }
         }
--- chapter07/src/main/java/nia/chapter07/ScheduleExamples.java
+++ chapter07/src/main/java/nia/chapter07/ScheduleExamples.java
@@ -22,16 +22,18 @@ public class ScheduleExamples {
      * Listing 7.2 Scheduling a task with a ScheduledExecutorService
      */
     public static void schedule() {
-        ScheduledExecutorService executor =
-                Executors.newScheduledThreadPool(10);
+        // 创建一个其线程池具有10 个线程的ScheduledExecutorService
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(10);
 
-        ScheduledFuture<?> future = executor.schedule(new Runnable() {
+        ScheduledFuture<?> future = executor.schedule(new Runnable() { // 创建一个 Runnable，以供调度稍后执行
             @Override
             public void run() {
+                // 该任务要打印的消息
                 System.out.println("Now it is 60 seconds later");
             }
-        }, 60, TimeUnit.SECONDS);
+        }, 60, TimeUnit.SECONDS); // 调度任务在从现在开始的60 秒之后执行
         //...
+        // 一旦调度任务执行完成，就关闭ScheduledExecutorService 以释放资源
         executor.shutdown();
     }
 
@@ -41,12 +43,13 @@ public class ScheduleExamples {
     public static void scheduleViaEventLoop() {
         Channel ch = CHANNEL_FROM_SOMEWHERE; // get reference from somewhere
         ScheduledFuture<?> future = ch.eventLoop().schedule(
-                new Runnable() {
+                new Runnable() { // 创建一个 Runnable，以供调度稍后执行
                     @Override
                     public void run() {
+                        // 要执行的代码
                         System.out.println("60 seconds later");
                     }
-                }, 60, TimeUnit.SECONDS);
+                }, 60, TimeUnit.SECONDS); // 调度任务在从现在开始的60 秒之后执行
     }
 
     /**
@@ -54,12 +57,14 @@ public class ScheduleExamples {
      */
     public static void scheduleFixedViaEventLoop() {
         Channel ch = CHANNEL_FROM_SOMEWHERE; // get reference from somewhere
+        // 创建一个 Runnable，以供调度稍后执行
         ScheduledFuture<?> future = ch.eventLoop().scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
+                // 这将一直运行，直到ScheduledFuture 被取消
                 System.out.println("Run every 60 seconds");
             }
-        }, 60, 60, TimeUnit.SECONDS);
+        }, 60, 60, TimeUnit.SECONDS); // 调度在60 秒之后，并且以后每间隔60 秒运行
     }
 
     /**
@@ -67,6 +72,7 @@ public class ScheduleExamples {
      */
     public static void cancelingTaskUsingScheduledFuture() {
         Channel ch = CHANNEL_FROM_SOMEWHERE; // get reference from somewhere
+        // 调度任务，并获得所返回的ScheduledFuture
         ScheduledFuture<?> future = ch.eventLoop().scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
@@ -75,6 +81,7 @@ public class ScheduleExamples {
         }, 60, 60, TimeUnit.SECONDS);
         // Some other code that runs...
         boolean mayInterruptIfRunning = false;
+        // 取消该任务，防止它再次运行
         future.cancel(mayInterruptIfRunning);
     }
 }
