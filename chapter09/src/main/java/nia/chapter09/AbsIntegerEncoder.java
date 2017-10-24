3fef0eec10073bbb52073d50188531eb09d2d6e6
王俊超 <shining-glory@qq.com>
2017-10-24 08:07:51 +0800

    commit

--- chapter09/src/main/java/nia/chapter09/AbsIntegerEncoder.java
+++ chapter09/src/main/java/nia/chapter09/AbsIntegerEncoder.java
@@ -13,11 +13,15 @@ import java.util.List;
  * Github: https://github.com/wang-jun-chao
  * All Rights Reserved !!!
  */
+// 扩展MessageToMessageEncoder 以将一个消息编码为另外一种格式
 public class AbsIntegerEncoder extends MessageToMessageEncoder<ByteBuf> {
     @Override
     protected void encode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        // 检查是否有足够的字节用来编码
         while (in.readableBytes() >= 4) {
+            // 从输入的ByteBuf中读取下一个整数，并且计算其绝对值
             int value = Math.abs(in.readInt());
+            // 将该整数写入到编码消息的List中
             out.add(value);
         }
     }
--- chapter09/src/test/java/nia/test/chapter09/FixedLengthFrameDecoderTest.java
+++ chapter09/src/test/java/nia/test/chapter09/FixedLengthFrameDecoderTest.java
@@ -17,19 +17,27 @@ import static org.junit.Assert.*;
  * All Rights Reserved !!!
  */
 public class FixedLengthFrameDecoderTest {
+    // 使用了注解@Test 标注，因此JUnit 将会执行该方法
     @Test
+    // 第一个测试方法：testFramesDecoded()
     public void testFramesDecoded() {
+        // 创建一个ByteBuf，并存储9 字节
         ByteBuf buf = Unpooled.buffer();
         for (int i = 0; i < 9; i++) {
             buf.writeByte(i);
         }
         ByteBuf input = buf.duplicate();
+        // 创建一个EmbeddedChannel，并添加一个FixedLengthFrameDecoder，其将以3 字节的帧长度被测试
         EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+
         // write bytes
+        // 将数据写入EmbeddedChannel
         assertTrue(channel.writeInbound(input.retain()));
+        // 标记Channel为已完成状态
         assertTrue(channel.finish());
 
         // read messages
+        // 读取所生成的消息，并且验证是否有3 帧（切片），其中每帧（切片）都为3 字节
         ByteBuf read = (ByteBuf) channel.readInbound();
         assertEquals(buf.readSlice(3), read);
         read.release();
@@ -47,6 +55,7 @@ public class FixedLengthFrameDecoderTest {
     }
 
     @Test
+    // 第二个测试方法：testFramesDecoded2()
     public void testFramesDecoded2() {
         ByteBuf buf = Unpooled.buffer();
         for (int i = 0; i < 9; i++) {
@@ -55,6 +64,7 @@ public class FixedLengthFrameDecoderTest {
         ByteBuf input = buf.duplicate();
 
         EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+        // 返回false，因为没有一个完整的可供读取的帧
         assertFalse(channel.writeInbound(input.readBytes(2)));
         assertTrue(channel.writeInbound(input.readBytes(7)));
 
