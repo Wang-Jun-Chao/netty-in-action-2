6b54be944dfa2218b200ea1f1d110da555b24a62
王俊超 <shining-glory@qq.com>
2017-10-26 08:20:57 +0800

    commit

--- chapter11/src/main/java/nia/chapter11/HttpsCodecInitializer.java
+++ chapter11/src/main/java/nia/chapter11/HttpsCodecInitializer.java
@@ -30,11 +30,14 @@ public class HttpsCodecInitializer extends ChannelInitializer<Channel> {
     protected void initChannel(Channel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
         SSLEngine engine = context.newEngine(ch.alloc());
+        // 将SslHandler 添加到ChannelPipeline 中以使用HTTPS
         pipeline.addFirst("ssl", new SslHandler(engine));
 
         if (isClient) {
+            // 如果是客户端，则添加HttpClientCodec
             pipeline.addLast("codec", new HttpClientCodec());
         } else {
+            // 如果是服务器，则添加HttpServerCodec
             pipeline.addLast("codec", new HttpServerCodec());
         }
     }
