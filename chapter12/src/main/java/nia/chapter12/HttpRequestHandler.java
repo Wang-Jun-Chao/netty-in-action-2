a9cd09df0b3b683d304b626528515c8c8dec302f
王俊超 <shining-glory@qq.com>
2017-10-28 07:54:48 +0800

    commit

--- chapter12/src/main/java/nia/chapter12/HttpRequestHandler.java
+++ chapter12/src/main/java/nia/chapter12/HttpRequestHandler.java
@@ -17,21 +17,19 @@ import java.net.URL;
  * Github: https://github.com/wang-jun-chao
  * All Rights Reserved !!!
  */
+// 扩展SimpleChannelInboundHandler 以处理FullHttpRequest 消息
 public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     private final String wsUri;
     private static final File INDEX;
 
     static {
-        URL location = HttpRequestHandler.class
-             .getProtectionDomain()
-             .getCodeSource().getLocation();
+        URL location = HttpRequestHandler.class.getProtectionDomain().getCodeSource().getLocation();
         try {
             String path = location.toURI() + "index.html";
             path = !path.contains("file:") ? path : path.substring(5);
             INDEX = new File(path);
         } catch (URISyntaxException e) {
-            throw new IllegalStateException(
-                 "Unable to locate index.html", e);
+            throw new IllegalStateException("Unable to locate index.html", e);
         }
     }
 
@@ -41,35 +39,39 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx,
-        FullHttpRequest request) throws Exception {
+            FullHttpRequest request) throws Exception {
+        // 如果请求了WebSocket协议升级，则增加引用计数（调用retain()方法），
+        // 并将它传递给下一个ChannelInboundHandler
         if (wsUri.equalsIgnoreCase(request.getUri())) {
             ctx.fireChannelRead(request.retain());
         } else {
+
+            // 处理100 Continue请求以符合HTTP 1.1 规范
             if (HttpHeaders.is100ContinueExpected(request)) {
                 send100Continue(ctx);
             }
+            // 读取index.html
             RandomAccessFile file = new RandomAccessFile(INDEX, "r");
-            HttpResponse response = new DefaultHttpResponse(
-                request.getProtocolVersion(), HttpResponseStatus.OK);
-            response.headers().set(
-                HttpHeaders.Names.CONTENT_TYPE,
-                "text/html; charset=UTF-8");
+            HttpResponse response = new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
             boolean keepAlive = HttpHeaders.isKeepAlive(request);
+            // 如果请求了keep-alive，则添加所需要的HTTP头信息
             if (keepAlive) {
-                response.headers().set(
-                    HttpHeaders.Names.CONTENT_LENGTH, file.length());
-                response.headers().set( HttpHeaders.Names.CONNECTION,
-                    HttpHeaders.Values.KEEP_ALIVE);
+                response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+                response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
             }
+            // 将HttpResponse写到客户端
             ctx.write(response);
+            // 将index.html写到客户端
             if (ctx.pipeline().get(SslHandler.class) == null) {
-                ctx.write(new DefaultFileRegion(
-                    file.getChannel(), 0, file.length()));
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
             } else {
                 ctx.write(new ChunkedNioFile(file.getChannel()));
             }
-            ChannelFuture future = ctx.writeAndFlush(
-                LastHttpContent.EMPTY_LAST_CONTENT);
+
+            // 写LastHttpContent并冲刷至客户端
+            ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+            // 如果没有请求keep-alive，则在写操作完成后关闭Channel
             if (!keepAlive) {
                 future.addListener(ChannelFutureListener.CLOSE);
             }
@@ -78,12 +80,13 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
 
     private static void send100Continue(ChannelHandlerContext ctx) {
         FullHttpResponse response = new DefaultFullHttpResponse(
-            HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+                HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
         ctx.writeAndFlush(response);
     }
+
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-        throws Exception {
+            throws Exception {
         cause.printStackTrace();
         ctx.close();
     }
