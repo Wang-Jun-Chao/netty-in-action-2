8bd015f428ac05dfcdee6cf6c30ad611e3ad9529
王俊超 <shining-glory@qq.com>
2017-10-28 07:11:54 +0800

    commit

--- chapter11/src/main/java/nia/chapter11/IdleStateHandlerInitializer.java
+++ chapter11/src/main/java/nia/chapter11/IdleStateHandlerInitializer.java
@@ -16,4 +16,36 @@ import java.util.concurrent.TimeUnit;
  * Github: https://github.com/wang-jun-chao
  * All Rights Reserved !!!
  */
+public class IdleStateHandlerInitializer extends ChannelInitializer<Channel> {
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(
+                // IdleStateHandler 将在被触发时发送一个IdleStateEvent 事件
+                new IdleStateHandler(0, 0, 60, TimeUnit.SECONDS));
+        // 将一个HeartbeatHandler添加到ChannelPipeline中
+        pipeline.addLast(new HeartbeatHandler());
+    }
+
+    // 实现userEventTriggered()方法以发送心跳消息
+    public static final class HeartbeatHandler
+            extends ChannelInboundHandlerAdapter {
+        // 发送到远程节点的心跳消息
+        private static final ByteBuf HEARTBEAT_SEQUENCE =
+                Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("HEARTBEAT", CharsetUtil.ISO_8859_1));
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx,
+                Object evt) throws Exception {
+            // 发送心跳消息，并在发送失败时关闭该连接
+            if (evt instanceof IdleStateEvent) {
+                ctx.writeAndFlush(HEARTBEAT_SEQUENCE.duplicate())
+                        .addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+            } else {
+                // 不是IdleStateEvent事件，所以将它传递给下一个ChannelInboundHandler
+                super.userEventTriggered(ctx, evt);
+            }
+        }
+    }
+}
 
