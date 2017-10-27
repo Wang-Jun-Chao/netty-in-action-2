a4e896ab156940a77d6b02659dfef09cadb807f7
王俊超 <shining-glory@qq.com>
2017-10-27 08:23:40 +0800

    commit

--- chapter11/src/main/java/nia/chapter11/LengthBasedInitializer.java
+++ chapter11/src/main/java/nia/chapter11/LengthBasedInitializer.java
@@ -16,15 +16,16 @@ public class LengthBasedInitializer extends ChannelInitializer<Channel> {
     protected void initChannel(Channel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
         pipeline.addLast(
+                // 使用LengthFieldBasedFrameDecoder 解码将帧长度编码到帧起始的前8 个字节中的消息
                 new LengthFieldBasedFrameDecoder(64 * 1024, 0, 8));
+        // 添加FrameHandler以处理每个帧
         pipeline.addLast(new FrameHandler());
     }
 
-    public static final class FrameHandler
-        extends SimpleChannelInboundHandler<ByteBuf> {
+    public static final class FrameHandler extends SimpleChannelInboundHandler<ByteBuf> {
+        // 处理帧的数据
         @Override
-        public void channelRead0(ChannelHandlerContext ctx,
-             ByteBuf msg) throws Exception {
+        public void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
             // Do something with the frame
         }
     }
